e73cfa2b287f90fc3746a1ff08930f1c469df2cc
Daryl Jewkes <github@daryljewkes.com>
2014-07-28 17:10:12 +0100

    building test cases, yes some do fail

--- js/tests.js
+++ js/tests.js
@@ -1,18 +1,25 @@
-// QUnit.test( "hello test", function( assert ) {
-//   assert.ok( 1 == "1", "Passed!" );
-// });
-
-
 QUnit.test("getTimeText basics", function (assert) {
 	assert.equal(getTimeText(new Date("1977/01/01 00:00:00")), "12 o'clock");
-    assert.equal(getTimeText(new Date("1977/01/01 22:25:00")), "25 minutes past 10");
+	assert.equal(getTimeText(new Date("1977/01/01 03:00:00")), "3 o'clock");
+
+	assert.equal(getTimeText(new Date("1977/01/01 03:45:00")), "quarter to 4");
+	assert.equal(getTimeText(new Date("1977/01/01 07:15:00")), "quarter past 7");
+    
+    assert.equal(getTimeText(new Date("1977/01/01 02:30:00")), "half past 2");
+    assert.equal(getTimeText(new Date("1977/01/01 08:30:00")), "half past 8");
 });
 
 QUnit.test("getTimeText complex", function (assert) {
 	assert.equal(getTimeText(new Date("1970/01/01 22:37:00")), "25 minutes to 11");
+	assert.equal(getTimeText(new Date("1977/01/01 10:25:00")), "25 minutes past 10");
 });
 
 QUnit.test("getTimeText ignoreSeconds", function (assert) {
+	assert.equal(getTimeText(new Date("1970/01/01 04:36:59")), "25 minutes to 5");
+	assert.equal(getTimeText(new Date("1970/01/01 04:37:01")), "25 minutes to 5");
+});
+
+QUnit.test("getTimeText ignoreTwentyFourHour", function (assert) {
+	assert.equal(getTimeText(new Date("1970/01/01 23:26:59")), "25 minutes past 11");
 	assert.equal(getTimeText(new Date("1970/01/01 22:36:59")), "25 minutes to 11");
-	assert.equal(getTimeText(new Date("1970/01/01 22:37:01")), "25 minutes to 11");
 });
